d6090e85bc1155131cefeac2e9cca22320ced530
Asliddin <98747380+eshonkulov-asliddin@users.noreply.github.com>
2023-09-12 10:53:31 +0500

    Time: 2 ms (89.58%), Space: 43.7 MB (50.84%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.java
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.java
@@ -1,9 +1,13 @@
 class Solution {
+    /*
+        Time Complexity: O(n+k)
+        Space Complexity: O(1)
+    */
     public int maximizeSum(int[] nums, int k) {
         
         int maxId = -1, maxNum = -1;
         
-        
+        // find max num and id
         for (int i = 0; i < nums.length; i++) {
             int curNum = nums[i];
             if (curNum > maxNum) {
