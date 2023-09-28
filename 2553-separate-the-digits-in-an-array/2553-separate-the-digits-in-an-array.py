ea3da26d51cca4e00b85cfb52568b3f8df269830
Asliddin <98747380+eshonkulov-asliddin@users.noreply.github.com>
2023-09-28 08:54:03 +0500

    Time: 5 ms (70.78%), Space: 44 MB (61.33%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
@@ -1,21 +1,33 @@
-class Solution:
-    def separateDigits(self, nums: List[int]) -> List[int]:
-        ans = []
-        for num in nums:
-            curDigits = []
-            while num:
-                remainder = num % 10
-                curDigits.append(remainder)
-                num //= 10
-            ans.extend(curDigits[::-1]) 
+class Solution {
+    public int[] separateDigits(int[] nums) {
         
-        return ans
-    
-"""
-    Time Complexity: O(n)
-    Space Complexity: O(n)
-    
-    n is the number of digits within an array
+        List<Integer> allDigits = new ArrayList<>();
+        
+        for (int i = nums.length-1; i >= 0; i--){
+            
+            int curNum = nums[i];
+            while (curNum != 0) {
+                int remainder = curNum % 10;
+                allDigits.add(remainder);
+                curNum /= 10;
+            }
+        }
+        
+        Collections.reverse(allDigits);
+        
+        int listSize = allDigits.size();
+        int[] ans = new int[listSize];
+        
+        for (int i = 0; i < listSize; i++){
+            ans[i] = allDigits.get(i);
+        }
+        
+        return ans;
+        
+    }
+}
 
-"""
-    
+/*
+    Time Complexity: O(n) n is the number of digits
+    Space Complexity: O(n) n is the number of digits
+*/
