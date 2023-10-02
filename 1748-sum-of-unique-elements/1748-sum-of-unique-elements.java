f965c623889762667c9d5f1ac47a48e0aeb57204
Asliddin <98747380+eshonkulov-asliddin@users.noreply.github.com>
2023-10-02 09:38:41 +0500

    Time: 1 ms (85.27%), Space: 40.8 MB (5.55%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    int sum = 0;
+    
+    public int sumOfUnique(int[] nums) {
+        
+        Map<Integer, Integer> numCount = new HashMap<>();
+        
+        for (int num : nums) {
+            numCount.put(num, numCount.getOrDefault(num, 0) + 1);
+        }
+        
+        
+        
+        numCount.forEach( (key, value) -> {
+            if (value == 1){
+                sum += key;
+            }
+        });
+        
+        return sum;   
+    }
+}
+
+/*
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+*/
