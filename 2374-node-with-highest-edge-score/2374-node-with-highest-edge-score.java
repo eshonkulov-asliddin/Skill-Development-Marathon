ad8bf2e1c9d615975326af7eb734bb3ca8eddb1a
Asliddin <98747380+eshonkulov-asliddin@users.noreply.github.com>
2023-11-13 18:32:27 +0500

    Time: 158 ms (10.56%), Space: 70.9 MB (6.34%) - LeetHub

---
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int edgeScore(int[] edges) {
+        
+        Map<Integer, List<Integer>> map = getDirectedMap(edges);
+        
+        return getMaxEdgeScore(map);
+        
+    }
+    
+    public int getMaxEdgeScore(Map<Integer, List<Integer>> map){
+        int maxNode = Integer.MIN_VALUE;
+        long maxEdge = 0;
+        
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()){
+            int node = entry.getKey();
+            List<Integer> edges = entry.getValue();
+            
+            long sumEdge = edges.stream().mapToLong(edge -> edge).sum();
+            
+            if ((sumEdge > maxEdge) || 
+                (sumEdge == maxEdge && node < maxNode)){
+                maxNode = node;
+                maxEdge = sumEdge;
+            }
+        }
+        
+        return maxNode;
+    }
+    
+    public Map<Integer, List<Integer>> getDirectedMap(int[] edges){
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        
+        int n = edges.length;
+        for (int i = 0; i < n; i++) {
+            int curVal = edges[i];
+
+            if (curVal != i) { // Add this condition
+                List<Integer> list = map.getOrDefault(curVal, new ArrayList());
+                list.add(i);
+
+                map.put(curVal, list);
+        }
+}
+
+        
+        return map;
+    }
+}
+
+/*
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+*/
