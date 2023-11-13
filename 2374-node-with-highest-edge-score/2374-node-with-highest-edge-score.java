4ab79944b4533149414a923c35551621b7c764a4
Asliddin <98747380+eshonkulov-asliddin@users.noreply.github.com>
2023-11-13 18:37:35 +0500

    Time: 110 ms (13.38%), Space: 70.5 MB (6.34%) - LeetHub

--- 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.java
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.java
@@ -1,52 +1,40 @@
+import java.util.*;
+
 class Solution {
     public int edgeScore(int[] edges) {
-        
         Map<Integer, List<Integer>> map = getDirectedMap(edges);
-        
         return getMaxEdgeScore(map);
-        
     }
-    
-    public int getMaxEdgeScore(Map<Integer, List<Integer>> map){
+
+    public int getMaxEdgeScore(Map<Integer, List<Integer>> map) {
         int maxNode = Integer.MIN_VALUE;
         long maxEdge = 0;
-        
-        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()){
+
+        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
             int node = entry.getKey();
-            List<Integer> edges = entry.getValue();
-            
-            long sumEdge = edges.stream().mapToLong(edge -> edge).sum();
-            
-            if ((sumEdge > maxEdge) || 
-                (sumEdge == maxEdge && node < maxNode)){
+            long sumEdge = entry.getValue().stream().mapToLong(Long::valueOf).sum();
+
+            if (sumEdge > maxEdge || (sumEdge == maxEdge && node < maxNode)) {
                 maxNode = node;
                 maxEdge = sumEdge;
             }
         }
-        
+
         return maxNode;
     }
-    
-    public Map<Integer, List<Integer>> getDirectedMap(int[] edges){
+
+    public Map<Integer, List<Integer>> getDirectedMap(int[] edges) {
         Map<Integer, List<Integer>> map = new HashMap<>();
-        
+
         int n = edges.length;
         for (int i = 0; i < n; i++) {
             int curVal = edges[i];
 
             if (curVal != i) {
-                List<Integer> list = map.getOrDefault(curVal, new ArrayList());
-                list.add(i);
-
-                map.put(curVal, list);
+                map.computeIfAbsent(curVal, k -> new ArrayList<>()).add(i);
             }
         }
 
         return map;
     }
 }
-
-/*
-    Time Complexity: O(n)
-    Space Complexity: O(n)
-*/
