2bf381537097c5a3bc26c39156b1bab306d5c32b
Asliddin <98747380+eshonkulov-asliddin@users.noreply.github.com>
2023-11-17 14:57:54 +0500

    Time: 0 ms (100.00%), Space: 43.4 MB (29.04%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1, mid = 0;
+        
+        while(l <= r){
+            mid = (l+r) / 2;
+            
+            if (nums[mid] == target){
+                return mid;
+            }else if (nums[mid] > target){
+                r = mid -1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        
+        return nums[mid] > target ? mid : mid+1;
+    }
+}
+
+/*
+    Time Complexity: O(log n)
+    Space Complexity: O(1)
+*/
