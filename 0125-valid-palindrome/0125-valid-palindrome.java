84c5674fb4e468ef9476657880003cde5c243041
Asliddin <98747380+eshonkulov-asliddin@users.noreply.github.com>
2024-01-24 12:42:25 +0500

    Time: 3 ms (75.86%), Space: 42.7 MB (74.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,33 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
-        StringBuilder word = new StringBuilder();
+        int start = 0;
+        int end = s.length() - 1;
         
-        for (char ch : s.toCharArray()) {
+        
+        while (start < end) {
             
-            if (Character.isLetter(ch) || Character.isDigit(ch)){
-                word.append(ch);
+            char startChar = s.charAt(start);
+            char endChar = s.charAt(end);
+            
+            if (!Character.isLetter(startChar) && !Character.isDigit(startChar)) {
+                start++;
+                continue;
             }
+            
+            if (!Character.isLetter(endChar) && !Character.isDigit(endChar)) {
+                end--;
+                continue;
+            }
+            
+            if (Character.toLowerCase(startChar) != Character.toLowerCase(endChar)){
+                return false;
+            }
+            
+            start++;
+            end--;
         }
         
-        String original = word.toString();
-        String reversed = word.reverse().toString();
-        
-        System.out.println(word);
-        System.out.println(reversed);
-        
-        return original.equalsIgnoreCase(reversed);
-        
+        return true;
     }
 }
