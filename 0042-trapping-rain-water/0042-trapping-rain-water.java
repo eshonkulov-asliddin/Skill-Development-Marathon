cf9db79b5285005008d1aa3d6df7a3169e671747
Asliddin <98747380+eshonkulov-asliddin@users.noreply.github.com>
2024-01-31 12:06:57 +0500

    Time: 0 ms (100.00%), Space: 46.8 MB (7.71%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -3,44 +3,33 @@ class Solution {
         
         int n = height.length;
         
-        int[] maxLeft = new int[n];
-        int[] maxRight = new int[n];
+        int maxLeft = height[0];
+        int maxRight = height[n-1];
         
-        // fill maxLeft
-        // first index always zero
-        maxLeft[0] = 0;
-        int curLeftMax = height[0];
-        for (int i = 1; i < n; i++) {
-            
-            curLeftMax = Math.max(curLeftMax, height[i-1]);
-            
-            maxLeft[i] = curLeftMax;
-        }
-        
-        
-        // fill maxRight
-        // last index always zero
-        maxLeft[n - 1] = 0;
-        int curRightMax = height[n - 1];
-        for (int i = n-2; i >= 0; i--) {
-            
-            curRightMax = Math.max(curRightMax, height[i+1]);
-            
-            maxRight[i] = curRightMax;
-        }
-        
+        int l = 0;
+        int r = n - 1;
         
         int count = 0;
         
-        for (int i = 0; i < n; i++) {
+        while (l <= r) {
             
-            int currHeight = height[i];
-            
-            int tmp = Math.min(maxLeft[i], maxRight[i]);
-            
-            int diff = tmp - currHeight;
-            if (diff > 0){
-                count += diff;
+            if (maxLeft <= maxRight) {
+                
+                int diff = maxLeft - height[l];
+                if (diff > 0) {
+                    count += diff;
+                }
+                
+                maxLeft = Math.max(maxLeft, height[l]);
+                l++;
+            } else {
+                int diff = maxRight - height[r];
+                if (diff > 0) {
+                    count+= diff;
+                }
+                
+                maxRight = Math.max(maxRight, height[r]);
+                r--;
             }
         }
         
